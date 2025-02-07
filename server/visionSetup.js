eb0b8205824c17429f1e7289937c40ebd77fc999
Arunava Chakrabarty <137680973+CapAru@users.noreply.github.com>
2025-02-08 01:24:06 +0530

    Added vision Model (Requires billing account)

--- server/visionSetup.js
+++ server/visionSetup.js
@@ -1,8 +1,28 @@
-import { ImageAnnotatorClient } from "@google-cloud/vision";
+import { ImageAnnotatorClient } from "@google-cloud/vision"
+import dotenv from "dotenv"
 
+dotenv.config(
+    {
+        path: "./.env"
+    }
+)
 const config = {
     credentials: {
-        private_key: process.env.VISION_API_PRIVATE_KEY.replace(/\\n/g, "\n"),
+        private_key: process.env.VISION_API_PRIVATE_KEY,
         client_email: process.env.VISION_API_CLIENT_EMAIL,
     },
-};
+}
+
+const client = new ImageAnnotatorClient(config);
+
+async function detectTextInImage(imagePath) {
+    const [result] = await client.textDetection(imagePath);
+    return result.textAnnotations[0].description;
+}
+
+const imagePath = "./Screenshot.png"
+const data = detectTextInImage(imagePath)
+    .then((text) => console.log(text))
+    .catch((err) => console.error(err))
+
+console.log(data)
