f48da6dc38b7105458b10652154e84a15696d1dd
Amritesh Kumar <157412986+AmriteshKr24@users.noreply.github.com>
2025-03-13 04:52:58 +0530

    herocomponent responsiveness

--- client/src/components/HeroComponent.jsx
+++ client/src/components/HeroComponent.jsx
@@ -1,31 +1,31 @@
 import { useUser } from "@/context/UserContext";
 import { getCookie } from "cookies-next";
-import { useEffect, useState } from "react";
+
 export default function HeroComponent() {
-    const { user, setUser } = useUser();
+    const { user } = useUser();
     const uid = getCookie("uid");
 
     return (
-        <div className="flex flex-col items-center p-6 bg-transparent text-black min-h-screen w-full">
-            <h1 className="text-4xl q text-blue-400 font-semibold text-center mb-6 mt-20">
+        <div className="flex flex-col items-center p-4 sm:p-6 bg-transparent text-black min-h-screen w-full">
+            <h1 className="text-3xl sm:text-4xl text-blue-400 font-semibold text-center mb-6 mt-16 sm:mt-20">
                 {`Welcome, ${user.name ? user.name : "Teacher"}!`}
             </h1>
-            <div className="flex flex-col items-center space-y-4">
+            <div className="flex flex-col items-center space-y-4 w-full max-w-xs sm:max-w-sm">
                 <a
                     href="/add-student"
-                    className="bg-teal-600 text-white py-2 px-4 rounded hover:bg-teal-700"
+                    className="w-full text-center bg-teal-600 text-white py-2 px-4 rounded-lg hover:bg-teal-700 transition duration-300"
                 >
                     Add Student
                 </a>
                 <a
                     href={`${uid}/studentlist`}
-                    className="bg-teal-600 text-white py-2 px-4 rounded hover:bg-teal-700"
+                    className="w-full text-center bg-teal-600 text-white py-2 px-4 rounded-lg hover:bg-teal-700 transition duration-300"
                 >
                     View Students List
                 </a>
                 <a
                     href="/evaluate"
-                    className="bg-teal-600 text-white py-2 px-4 rounded hover:bg-teal-700"
+                    className="w-full text-center bg-teal-600 text-white py-2 px-4 rounded-lg hover:bg-teal-700 transition duration-300"
                 >
                     Evaluate
                 </a>
