eb74f058b0189a4c320a448ce24dd5df16d8bca9
Harsh-Prasad09 <immortalharsh61@outlook.com>
2025-03-19 00:17:25 +0530

    improved button colors for herocomponent

--- client/src/components/StudentBox.jsx
+++ client/src/components/StudentBox.jsx
@@ -6,13 +6,13 @@ export default function StudentBox({ studentId }) {
       <p className="text-black dark:text-white text-center">Student ID: {studentId}</p>
       <div className="flex flex-col space-y-2 w-full items-center">
         <a
-          className="inline-block bg-primary dark:bg-blue-600 text-white py-2 px-4 rounded hover:bg-secondary dark:hover:bg-blue-700 w-full text-center"
+          className="inline-block bg-[#1d2f6f] dark:bg-blue-600 text-white py-2 px-4 rounded hover:bg-[#263f95] dark:hover:bg-blue-700 w-full text-center"
           href={`/student/${studentId}/evaluate`}
         >
           Evaluate
         </a>
         <a
-          className="inline-block bg-primary dark:bg-blue-600 text-white py-2 px-4 rounded hover:bg-secondary dark:hover:bg-blue-700 w-full text-center"
+          className="inline-block bg-[#1d2f6f] dark:bg-blue-600 text-white py-2 px-4 rounded hover:bg-[#263f95] dark:hover:bg-blue-700 w-full text-center"
           href={`/student/${studentId}/feedbacks`}
         >
           Feedback History
