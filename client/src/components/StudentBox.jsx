7728bdae37b720696de6f08181dce369e2fb9e54
MANIK <2022itb100.manik@students.iiests.ac.in>
2025-03-18 13:52:50 +0530

    Css fixes

--- client/src/components/StudentBox.jsx
+++ client/src/components/StudentBox.jsx
@@ -2,17 +2,17 @@ import React from "react";
 
 export default function StudentBox({ studentId }) {
   return (
-    <div className="bg-white rounded-lg shadow-md py-4 px-6 md:flex items-center justify-between md:w-2/3 mx-auto">
-      <p className="text-black">Student ID: {studentId}</p>
-      <div>
+    <div className="bg-white dark:bg-gray-800 rounded-lg shadow-md py-4 px-6 flex flex-col items-center justify-center w-full md:w-2/3 mx-auto space-y-4">
+      <p className="text-black dark:text-white text-center">Student ID: {studentId}</p>
+      <div className="flex flex-col space-y-2 w-full items-center">
         <a
-          className="inline-block bg-primary text-white py-2 px-4 rounded hover:bg-secondary mr-2"
+          className="inline-block bg-primary dark:bg-blue-600 text-white py-2 px-4 rounded hover:bg-secondary dark:hover:bg-blue-700 w-full text-center"
           href={`/student/${studentId}/evaluate`}
         >
           Evaluate
         </a>
         <a
-          className="inline-block bg-primary text-white py-2 px-4 rounded hover:bg-secondary"
+          className="inline-block bg-primary dark:bg-blue-600 text-white py-2 px-4 rounded hover:bg-secondary dark:hover:bg-blue-700 w-full text-center"
           href={`/student/${studentId}/feedbacks`}
         >
           Feedback History
