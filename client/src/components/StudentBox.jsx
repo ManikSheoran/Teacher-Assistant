88ac490290fff60935cff1d00236bee50087ee75
Harsh-Prasad09 <immortalharsh61@outlook.com>
2025-03-18 23:58:25 +0530

    css updation student list

--- client/src/components/StudentBox.jsx
+++ client/src/components/StudentBox.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function StudentBox({ studentId }) {
   return (
-    <div className="bg-white dark:bg-gray-800 rounded-lg shadow-md py-4 px-6 flex flex-col items-center justify-center w-full md:w-2/3 mx-auto space-y-4">
+    <div className="bg-sky-100 dark:bg-gray-800 rounded-lg shadow-md py-4 px-6 flex flex-col items-center justify-center w-full md:w-2/3 mx-auto space-y-4">
       <p className="text-black dark:text-white text-center">Student ID: {studentId}</p>
       <div className="flex flex-col space-y-2 w-full items-center">
         <a
