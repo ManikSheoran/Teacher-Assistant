a0df9242bf55fb141624869c3e283402be22bc3c
MANIK <136956963+ManikSheoran@users.noreply.github.com>
2025-07-16 16:36:02 +0530

    Add vercel analytics

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,10 +1,11 @@
 "use client";
 import React from "react";
 import IntroComponent from "@/components/IntroComponent";
-
+import { Analytics } from "@vercel/analytics/next"
 const MainPage = () => {
     return (
         <>
+        <Analytics />
             <IntroComponent />
         </>
     );
